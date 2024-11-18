d9e31cdbb6c74768296e585119276cd426f66a94
rangsariteh <76443168+rangsariteh@users.noreply.github.com>
2024-11-18 16:03:57 +0100

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { DEFAULT_CURRENCY_CODE, enableProdMode, LOCALE_ID } from '@angular/core';
+
+import { environment } from './environments/environment';
+import locale from '@angular/common/locales/de-CH';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { PreloadAllModules, provideRouter, RouteReuseStrategy, TitleStrategy, withPreloading } from '@angular/router';
+import appRoutes from './app/shared/app.routes';
+import { registerLocaleData } from '@angular/common';
+import { PageTitleStrategy } from './app/shared/service/page-title-strategy.service';
+import AppComponent from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { authInterceptor } from './app/shared/interceptor/auth.interceptor';
+
+if (environment.backendURL) enableProdMode();
+
+registerLocaleData(locale);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'CHF' },
+    { provide: LOCALE_ID, useValue: 'de-CH' },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: TitleStrategy, useClass: PageTitleStrategy },
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(),
+    provideIonicAngular(),
+    provideRouter(appRoutes, withPreloading(PreloadAllModules))
+  ]
+}).catch(err => console.error(err));
