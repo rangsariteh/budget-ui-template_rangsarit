61d06a088ddd69b82ac2fd44bc529f7df8e7efd2
rangsariteh <76443168+rangsariteh@users.noreply.github.com>
2024-11-18 15:37:47 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Budget UI',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
